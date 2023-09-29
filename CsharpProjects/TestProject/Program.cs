eecc944fd9c4e6a9e5ee8579c4327965c19001f2
breno085 <br3noacioli@gmail.com>
2023-09-29 00:22:27 -0300

    Created readme

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, C#!");
+﻿Random random = new Random();
+int daysUntilExpiration = random.Next(12);
+int discountPercentage = 0;
+
+if (daysUntilExpiration == 0) {
+    Console.WriteLine("Your subscription has expired.");
+}
+else if (daysUntilExpiration == 1){
+    Console.WriteLine("Your subscription expires within a day! Renew now and save 20%!");
+}
+else if (daysUntilExpiration <= 5){
+    Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days. Renew now and save 10%!");
+} 
+else if (daysUntilExpiration <= 10) {
+    Console.WriteLine("Your subscription will expire soon. Renew now!");
+}
+
+
+
