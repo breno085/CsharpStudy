1e8cfd0d4b7ce57fddb4323328eea9a596a7c999
breno085 <br3noacioli@gmail.com>
2023-10-03 10:09:33 -0300

    code project 1 that validates string inputs

--- CsharpProjects/TestProject/Program.cs
+++ CsharpProjects/TestProject/Program.cs
@@ -1,19 +1,73 @@
-﻿Random random = new Random();
-int daysUntilExpiration = random.Next(12);
-int discountPercentage = 0;
+﻿// ?: is the condicional operator, also know as the ternary condicional operator
+// <evaluate this condition> ? <if condition is true, return this value> : <if condition is false, return this value>
 
-if (daysUntilExpiration == 0) {
-    Console.WriteLine("Your subscription has expired.");
-}
-else if (daysUntilExpiration == 1){
-    Console.WriteLine("Your subscription expires within a day! Renew now and save 20%!");
-}
-else if (daysUntilExpiration <= 5){
-    Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days. Renew now and save 10%!");
+// int saleAmount = 1001;
+// // int discount = saleAmount > 1000 ? 100 : 50;
+// Console.WriteLine($"Discount: {(saleAmount > 1000 ? 100 : 50)}");
+
+// Random coin = new Random();
+// int flip = coin.Next(1, 3);
+// Console.WriteLine($"Result: {(flip > 1 ? "tails" : "heads")}");
+
+// string permission = "Admin|Manager";
+// int level = 55;
+
+// if (permission.Contains("Admin"))
+//     Console.WriteLine(level > 55 ? "Welcome, Super Admin user." : "Welcome, Admin user");
+// else if (permission.Contains("Manager"))
+//     Console.WriteLine(level >= 20 ? "Contact an Admin for access." : "You do not have sufficient privileges.");
+// else
+//     Console.WriteLine("You do not have sufficient privileges.");
+
+// int heroHealth = 10;
+// int monsterHealth = 10;
+// int damage = 0;
+
+// Random atack = new Random();
+
+// while (true)
+// {   
+//     if (heroHealth > 0)
+//     {   damage = atack.Next(1, 11);
+//         monsterHealth -= damage;
+//         Console.WriteLine($"Monster was damaged and lost {damage} health and now has {monsterHealth} health");
+//     } else
+//     {   Console.WriteLine("Monster Wins!");
+//         break;
+//     }
+//     if (monsterHealth > 0)
+//     {   damage = atack.Next(1, 11);
+//         heroHealth -= damage;
+//         Console.WriteLine($"Hero was damaged and lost {damage} health and now has {heroHealth} health");
+//     } else
+//     {   Console.WriteLine("Hero Wins!");
+//         break;
+//     }
+
+// }
+
+//Code project 1 - write code that validates integer input
+
+bool validEntry = false;
+string? answer;
+int userNumber = 0;
+
+Console.WriteLine("Enter a number between 5 and 10");
+
+while (validEntry == false)
+{
+    answer = Console.ReadLine();
+
+    if (int.TryParse(answer, out userNumber))
+    {   
+        if (!(userNumber > 5 && userNumber < 10))
+            Console.WriteLine($"You entered {userNumber}. Please enter a number between 5 and 10.");
+        else
+            validEntry = true;
+    } else
+        Console.WriteLine("Sorry, you entered a invalid number, please try again");
 } 
-else if (daysUntilExpiration <= 10) {
-    Console.WriteLine("Your subscription will expire soon. Renew now!");
-}
+Console.WriteLine($"You typed a valid ({userNumber}) integer.");
 
-
+//Code project 2 - write code that validates string input
 
